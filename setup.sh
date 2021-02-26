aa5e5cb032e9541e15f80c501be9ed10c32da7ee
Jose Ananias Hilario Reyes <j.ananias.hilario@gmail.com>
2021-02-26 12:59:27 +0100

    Modifications to build wheels.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 python setup.py install
+python setup.py sdist bdist_wheel
 
 cd ./cuda/block_extractor
 python setup.py install
+python setup.py sdist bdist_wheel
 
 cd ../local_attn_reshape
 python setup.py install
+python setup.py sdist bdist_wheel
 
 cd ../resample2d_package
 python setup.py install
+python setup.py sdist bdist_wheel
