117f593783da67da9dc910a751910760497ef37f
Jose Ananias Hilario Reyes <j.ananias.hilario@gmail.com>
2021-02-26 18:21:54 +0100

    Adding missing packages.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Build setup
 setup(
     name='ffwm',
-    version='1.0.0',
+    version='1.0.1',
     packages=['ffwm'],
     license='MIT License',
     author='Yuxiang Wei',
@@ -16,7 +16,16 @@ setup(
     setup_requires=[
         'setuptools>=18.0'
     ],
-    package_data={},
+    package_data={
+        'ffwm': [
+            'data/*',
+            'data_process/*',
+            'lightcnn/*',
+            'models/*',
+            'options/*',
+            'util/*',
+        ]
+    },
     install_requires=[],  # requirements.txt is included
     include_package_data=True,
 )
